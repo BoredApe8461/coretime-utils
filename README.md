abe136791255f8d36379416c852ce0378309e08e
BoredApe8461 <letesinkelegesse@gmail.com>
2024-01-12 09:55:35 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# coretime-utils
--- src/common.ts
+++
@@ -1,8 +0,0 @@
-export type CoreMask = string;
-
-export const FULL_MASK = "0xFFFFFFFFFFFFFFFFFFFF"; // hex encoded 80 bit bitmap.
-export const HALF_FULL_MASK = "0xFFFFFFFFFF0000000000"; // hex encoded 80 bit bitmap.
-
-export type Instruction =
-  | { kind: 'Partition'; pivot: number; ownPart: 0 | 1 }
-  | { kind: 'Interlace'; ownMask: CoreMask };
