6bfe09712761ec67fd995b4556b5b50827f536eb
BoredApe8461 <letesinkelegesse@gmail.com>
2024-05-07 13:55:52 +0200

    Remove CoreMask class

--- __tests__/coremask.test.ts
+++ __tests__/coremask.test.ts
@@ -1,51 +1,53 @@
-import { CoreMask, COMPLETE_MASK, VOID_MASK } from '../src';
+import { COMPLETE_MASK, VOID_MASK, completeMask, countMaskOnes, countMaskZeros, maskFromBin, maskFromChunk, maskToBin, voidMask } from '../src';
 
 describe('CoreMask utils work', () => {
-  test('constructor works', () => {
-    expect(() => new CoreMask(COMPLETE_MASK)).not.toThrow();
-    expect(() => new CoreMask(VOID_MASK)).not.toThrow();
-    expect(() => new CoreMask('foo')).toThrow();
-    expect(() => new CoreMask(VOID_MASK + '0')).toThrow();
-  });
-
   test('fromChunk works', () => {
-    expect(CoreMask.fromChunk(40, 60).toRawHex()).toEqual('0x0000000000fffff00000');
+    expect(maskFromChunk(40, 60)).toEqual('0x0000000000fffff00000');
   });
 
   test('fromBin works', () => {
     expect(
-      CoreMask.fromBin(
+      maskFromBin(
         '11111111111111111111111111111111111111111111111111111111111111111111111111111111',
-      ).toRawHex(),
+      ),
     ).toEqual(COMPLETE_MASK);
 
     expect(
-      CoreMask.fromBin(
+     maskFromBin(
         '00000000000000000000000000000000000000000000000000000000000000000000000000000000',
-      ).toRawHex(),
+      ),
     ).toEqual(VOID_MASK);
   });
 
   test('countOnes works', () => {
-    expect(CoreMask.voidMask().countOnes()).toEqual(0);
-    expect(CoreMask.completeMask().countOnes()).toEqual(80);
-    expect(CoreMask.fromChunk(0, 20).countOnes()).toEqual(20);
+    const _voidMask = voidMask();
+    const _completeMask = completeMask();
+    const _customMask = maskFromChunk(0, 20);
+    expect(countMaskOnes(_voidMask)).toEqual(0);
+    expect(countMaskOnes(_completeMask)).toEqual(80);
+    expect(countMaskOnes(_customMask)).toEqual(20);
   });
 
   test('countZeros works', () => {
-    expect(CoreMask.voidMask().countZeros()).toEqual(80);
-    expect(CoreMask.completeMask().countZeros()).toEqual(0);
-    expect(CoreMask.fromChunk(0, 20).countZeros()).toEqual(60);
+    const _voidMask = voidMask();
+    const _completeMask = completeMask();
+    const _customMask = maskFromChunk(0, 20);
+    expect(countMaskZeros(_voidMask)).toEqual(80);
+    expect(countMaskZeros(_completeMask)).toEqual(0);
+    expect(countMaskZeros(_customMask)).toEqual(60);
   });
 
   test('toBin works', () => {
-    expect(CoreMask.voidMask().toBin()).toEqual(
+    const _voidMask = voidMask();
+    const _completeMask = completeMask();
+    const _customMask = maskFromChunk(0, 5);
+    expect(maskToBin(_voidMask)).toEqual(
       '00000000000000000000000000000000000000000000000000000000000000000000000000000000',
     );
-    expect(CoreMask.completeMask().toBin()).toEqual(
+    expect(maskToBin(_completeMask)).toEqual(
       '11111111111111111111111111111111111111111111111111111111111111111111111111111111',
     );
-    expect(CoreMask.fromChunk(0, 5).toBin()).toEqual(
+    expect(maskToBin(_customMask)).toEqual(
       '11111000000000000000000000000000000000000000000000000000000000000000000000000000',
     );
   });
